664d17099d677901e8213480a82d752526e50c29
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-01 23:13:37 +0530

    GENERICS IN CLASSES

--- index.ts
+++ index.ts
@@ -421,4 +421,38 @@ const getSearchProductsArrowFunc = <T>(products: T[]) : T => {
   return products[myIndex]
 }
 
+//*********GENERICS IN CLASSES*********** */
+interface Database{
+  connection: string,
+  username: string,
+  password: string,
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U) : object{
+  return {
+    valOne,
+    valTwo
+  }
+}
+
+anotherFunction(3, {connection: 'conn', username: 'user', password: 'p'})
+
+interface Quiz{
+  name: string,
+  type: string
+}
+
+interface Course{
+  name: string,
+  author: string,
+  subject: string
+}
+
+class Sellable<T>{
+  public cart: T[] = []
+  addToCart(product: T){
+    this.cart.push(product)
+  }
+}
+
 export {};
