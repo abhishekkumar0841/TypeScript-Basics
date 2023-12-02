b8f6f6f8bbe81cdc7180e5b1032d28d5a633e9a3
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-02 16:42:26 +0530

    DISCRIMINATED UNION AND EXHAUSTIVENESS CHECKING WITH NEVER

--- index.ts
+++ index.ts
@@ -532,4 +532,45 @@ function getFood(pet: Fish | Bird) {
   }
 }
 
+//******DISCRIMINATED UNION AND EXHAUSTIVENESS CHECKING WITH NEVER******* */
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+      break;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default:
+      const defaultForShape: never = shape;
+      return defaultForShape;
+  }
+}
+
 export {};
